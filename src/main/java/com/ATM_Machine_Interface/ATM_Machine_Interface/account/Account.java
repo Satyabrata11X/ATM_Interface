7901996e66290de4cb22f94831f8f4e58e79ca27
satyabrata11x <satyabrata11x@gmail.com>
2026-06-15 08:40:29 +0530

    Account entity class .

--- src/main/java/com/ATM_Machine_Interface/ATM_Machine_Interface/account/Account.java
+++ src/main/java/com/ATM_Machine_Interface/ATM_Machine_Interface/account/Account.java
@@ -1,4 +1,21 @@
 package com.ATM_Machine_Interface.ATM_Machine_Interface.account;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
