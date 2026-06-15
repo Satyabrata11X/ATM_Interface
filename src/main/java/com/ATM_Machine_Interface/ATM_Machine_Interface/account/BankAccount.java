ccd7b51ba963480c5718415efc310c28fe2d554f
satyabrata11x <satyabrata11x@gmail.com>
2026-06-15 16:51:45 +0530

    Create BankAccount entity with JPA mappings and constraints

---
+++ src/main/java/com/ATM_Machine_Interface/ATM_Machine_Interface/account/BankAccount.java
@@ -0,0 +1,41 @@
+package com.ATM_Machine_Interface.ATM_Machine_Interface.account;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class BankAccount {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String accountNumber;
+
+    @Column(nullable = false)
+    private String accountHolderName;
+
+    @Column(nullable = false)
+    private Double balance;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+}
