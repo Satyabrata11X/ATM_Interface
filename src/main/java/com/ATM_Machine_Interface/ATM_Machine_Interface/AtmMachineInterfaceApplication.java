ccd098ffecc585e0d74d0a2e7cb68d5ee574cd8f
satyabrata11x <satyabrata11x@gmail.com>
2026-06-15 08:28:45 +0530

    Intial SpringBoot ATM project Setup

---
+++ src/main/java/com/ATM_Machine_Interface/ATM_Machine_Interface/AtmMachineInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.ATM_Machine_Interface.ATM_Machine_Interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmMachineInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmMachineInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ATM_Machine_Interface/ATM_Machine_Interface/AtmMachineInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ATM_Machine_Interface.ATM_Machine_Interface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmMachineInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
