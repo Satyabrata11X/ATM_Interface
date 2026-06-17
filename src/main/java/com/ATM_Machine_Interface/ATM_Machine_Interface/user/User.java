ac51c695c1fff0be7573225ad2caf90ebae4b526
satyabrata11x <satyabrata11x@gmail.com>
2026-06-17 11:26:12 +0530

    Create user entity and repository for authentication module

---
+++ src/main/java/com/ATM_Machine_Interface/ATM_Machine_Interface/user/User.java
@@ -0,0 +1,30 @@
+package com.ATM_Machine_Interface.ATM_Machine_Interface.user;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+}
---
+++ src/main/java/com/ATM_Machine_Interface/ATM_Machine_Interface/user/UserService.java
@@ -0,0 +1,4 @@
+package com.ATM_Machine_Interface.ATM_Machine_Interface.user;
+
+public class UserService {
+}
