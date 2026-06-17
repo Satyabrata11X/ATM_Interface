e86dc85b332c8a1849af5a4a36d76969476a72a0
satyabrata11x <satyabrata11x@gmail.com>
2026-06-17 11:29:18 +0530

    Add BCrypt password encoder configuration

---
+++ src/main/java/com/ATM_Machine_Interface/ATM_Machine_Interface/user/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.ATM_Machine_Interface.ATM_Machine_Interface.user;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
