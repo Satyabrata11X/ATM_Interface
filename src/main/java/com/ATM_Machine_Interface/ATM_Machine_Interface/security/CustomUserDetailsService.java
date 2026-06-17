fb573072571335c7e6182b147d1d08804882bed9
satyabrata11x <satyabrata11x@gmail.com>
2026-06-17 11:55:25 +0530

    Implement custom user details service for Spring Security

--- src/main/java/com/ATM_Machine_Interface/ATM_Machine_Interface/security/CustomUserDetailsService.java
+++ src/main/java/com/ATM_Machine_Interface/ATM_Machine_Interface/security/CustomUserDetailsService.java
@@ -1,4 +1,38 @@
 package com.ATM_Machine_Interface.ATM_Machine_Interface.security;
 
-public class CustomUserDetailsService {
+import com.ATM_Machine_Interface.ATM_Machine_Interface.user.User;
+import com.ATM_Machine_Interface.ATM_Machine_Interface.user.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService
+        implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(
+            UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(
+            String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository
+                .findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().replace("ROLE_", ""))
+                .build();
+    }
 }
